00e031390de776ca85a87018c9749d25f108780e
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-02-01 16:22:51 -0600

    Implement confirmation utility

---
+++ utils/confirmation.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+: '
+  Request confirmation from user
+
+  This is a [yY ]/[nN] confirmation, any other response will re-run the confirmation
+
+  @param  message [$1]  The confirmation message to display
+'
+confirm() {
+  read -n 1 -r -p "$1 [y/n] " ANSWER
+
+  if [[ "${ANSWER:-"y"}" =~ ^[yY]$ ]]; then
+    return 0
+  fi
+
+  if [[ "$ANSWER" =~ ^[nN]$ ]]; then
+    return 1
+  fi
+
+  (echo "" && confirm "$1")
+}
