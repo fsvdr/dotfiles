d7e7761c4078bae7c1877ff5ca0e7ba72448ffcb
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-02-03 16:40:38 -0600

    Add environment configuration step

---
+++ scripts/environment-configure.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+: '
+  Installs the development environment dependencies through Volta.sh and Homebrew
+'
+environment-configure() {
+  type volta &>/dev/null
+  if [[ $? -eq 1 ]]; then
+    echo -ne "  ⏬ Installing Volta\n"
+    curl https://get.volta.sh | bash
+  fi
+
+  echo -ne "\n  ⏬ Installing Volta toolkit\n"
+  volta install {"node","yarn","bash-language-server","parcel-bundler","gatsby-cli"}
+
+  type brew &>/dev/null
+  if [[ $? -eq 1 ]]; then
+    echo -ne "\n  ⏬ Installing Homebrew\n"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+
+  echo -ne "\n  ⏬ Installing Homebrew dependencies\n"
+  brew install {"git-lfs","mas","php","python3"}
+
+  return 0
+}
