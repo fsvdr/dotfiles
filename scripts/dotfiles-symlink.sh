13405d9a0846a71db0749ef8fc16aeeb3e40900a
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-02-04 20:35:21 -0600

    Fix symlink path

--- scripts/dotfiles-symlink.sh
+++ scripts/dotfiles-symlink.sh
@@ -4,7 +4,7 @@
   Creates symbolic links to every *.symlink file inside the dots directory
 '
 dotfiles-symlink() {
-  for src in $(find -H "${0%/*}/dots" -name "*.symlink"); do
+  for src in $(find -H "~/.dotfiles/dots" -name "*.symlink"); do
     local dst="$HOME/.$(basename "${src%.*}")"
     ln -sfv $src $dst 2>/dev/null >&2
   done
